cc0b22863faf57bb89fc21a45d31ce0fece6d4a7
Paolo <u20221f613@upc.edu.pe>
2025-04-27 17:04:05 -0500

    chore: configure HTTP client and translation service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import {HttpClient} from '@angular/common/http';
+i
+import {HttpClient, provideHttpClient} from '@angular/common/http';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {provideTranslateService} from '@ngx-translate/core';
 
 
 const httpLoaderFactory: (http: HttpClient) =>
@@ -14,5 +13,13 @@ const httpLoaderFactory: (http: HttpClient) =>
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)]
+    provideHttpClient(),
+    provideTranslateService({
+      loader: {
+        provide: TranslateHttpLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'en',
+    })]
 };
