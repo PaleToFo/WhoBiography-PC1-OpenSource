b4b7d0f412b351adaa1a1aa6f071e8f819a27637
Paolo <u20221f613@upc.edu.pe>
2025-04-27 20:09:13 -0500

    feat(sources): add source entity.

---
+++ src/app/biography/model/source.entity.ts
@@ -0,0 +1,19 @@
+export class Source {
+  id: string;
+  lastModified: string;
+  publicationDate: string;
+  dateCreated: string;
+  biography: string;
+  title: string;
+  provider: string;
+
+  constructor() {
+    this.id='';
+    this.lastModified='';
+    this.publicationDate='';
+    this.dateCreated='';
+    this.biography='';
+    this.title='';
+    this.provider='';
+  }
+}
